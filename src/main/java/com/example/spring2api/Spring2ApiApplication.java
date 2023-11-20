bd6483753367373744fe56a91224c2611b7f385f
sergejsokolov <sokolov.kstovo@gmail.com>
2023-11-20 11:08:41 +0300

    Initial commit

---
+++ src/main/java/com/example/spring2api/Spring2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2ApiApplication.class, args);
+    }
+
+}
